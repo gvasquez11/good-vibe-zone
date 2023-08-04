bbd8b455b2b7ff36f782c5c2bc7ed8b83afbcefc
gvasquez11 <vasquez.greg16@gmail.com>
2023-08-03 19:53:18 -0700

    added login and signup pages

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,11 @@
 module.exports = {
     homePage: (req,res) => {
         res.render('index.ejs')
+    },
+    loginPage: (req,res) =>{
+        res.render('signin.ejs')
+    },
+    signupPage: (req,res) => {
+        res.render('signup.ejs')
     }
 }
--- routes/main.js
+++ routes/main.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const mainController = require('../controllers/main')
 
 router.get('/', mainController.homePage)
+router.get('/login', mainController.loginPage)
+router.get('/signup', mainController.signupPage)
 
 module.exports = router
